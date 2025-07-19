ac0827159f32586c165771d023e609d374db5486
Harsh Kumar <harshofficial1905@gmail.com>
2025-07-19 15:49:23 +0530

    Time: 11 ms (5.92%) | Memory: 49.9 MB (27.54%) - LeetSync

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -1,13 +1,22 @@
-
+// not to compare by value , compare nodes 
 class Solution {
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(cloned);
-        while(!q.isEmpty()){
-            TreeNode temp = q.poll();
-            if(temp.val==target.val) return temp;
-            if(temp.left!=null) q.add(temp.left);
-            if(temp.right!=null) q.add(temp.right);
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.add(original);
+        q2.add(cloned);
+        while(!q1.isEmpty()){
+            TreeNode temp1 =q1.poll();
+            TreeNode temp2 = q2.poll();
+            if(temp1==target) return temp2;
+            if(temp1.left!=null){
+                    q1.add(temp1.left);
+                    q2.add(temp2.left);
+            }
+            if(temp1.right!=null){
+                    q1.add(temp1.right);
+                    q2.add(temp2.right);
+            }
         }
         return null;
     }
