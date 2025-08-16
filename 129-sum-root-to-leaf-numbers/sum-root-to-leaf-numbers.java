c7f3603f0a4b435b0e6827d63f1e07117af17af4
Harsh Kumar <harshofficial1905@gmail.com>
2025-08-17 01:51:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (31.33%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,30 +1,12 @@
-
+// OPTIMIZED APPROACH 
 class Solution {
-    ArrayList<ArrayList<Integer>>ans = new ArrayList<>();
-    public void helper(TreeNode root,ArrayList<Integer> list){
-        if(root==null) return ;
-        ArrayList<Integer> newlist = new ArrayList<>(list);
-        newlist.add(root.val);
-        if(root.left==null && root.right==null){
-            ans.add(newlist);
-            return ;
-        } 
-        helper(root.left,newlist);
-        helper(root.right,newlist);
+    public int dfs(TreeNode root, int sum){
+        if(root==null) return 0;
+        sum = (sum*10) +root.val;
+        if(root.left==null && root.right==null) return sum;
+        return (dfs(root.left,sum) + dfs(root.right,sum));
     }
     public int sumNumbers(TreeNode root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        helper(root,list);
-        int total = 0;
-        for(int i=0;i<ans.size();i++){
-            ArrayList temp = ans.get(i);
-            int sum =0;
-            for(int j=0;j<temp.size();j++){
-                int t = (int)temp.get(j);
-                sum = (sum*10) + t;
-            }
-            total +=sum;
-        }
-        return total;
+      return dfs(root,0);  
     }
 }
