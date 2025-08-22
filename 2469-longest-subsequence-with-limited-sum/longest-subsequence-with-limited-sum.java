f935f0b5ab711e7e5db8ffdbb186b920326f326c
Harsh Kumar <harshofficial1905@gmail.com>
2025-08-23 02:40:19 +0530

    Time: 5 ms (99.59%) | Memory: 44.8 MB (85.56%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -1,19 +1,23 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
-        int ans[] = new int[queries.length];
-        Arrays.sort(nums);
-        int prefix [] = new int[nums.length];
-        prefix[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            prefix[i] = prefix[i-1] + nums[i];
-        }
-        for(int i=0;i<queries.length;i++){
-            int j=0;
-            while(prefix[j]<=queries[i]){
-                j++;
-                if(j==prefix.length) break;
+       int ans[] = new int[queries.length];
+       Arrays.sort(nums);
+       int prefix[] = new int[nums.length];
+       prefix[0] = nums[0];
+       for(int i=1;i<nums.length;i++){
+        prefix[i] = prefix[i-1]+nums[i];
+       } 
+       for(int i=0;i<queries.length;i++){
+        int low = 0;
+        int high = nums.length;
+            while(low<high){
+                int mid = (low + high)/2;
+                if( prefix[mid]<=queries[i] ){
+                    low=mid+1;
+                }
+                else high = mid;
             }
-            ans[i] =j;
+        ans[i] = low;
         }
         return ans;
     }
