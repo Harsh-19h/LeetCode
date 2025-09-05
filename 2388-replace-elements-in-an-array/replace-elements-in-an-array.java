09a96e38ce3ae437be9fdb71ccafea8f0530dfd4
Harsh Kumar <harshofficial1905@gmail.com>
2025-09-05 15:19:06 +0530

    Time: 56 ms (12.96%) | Memory: 86.1 MB (88.04%) - LeetSync

--- 2388-replace-elements-in-an-array/replace-elements-in-an-array.java
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.java
@@ -1,23 +1,18 @@
+// OPTIMIZED CODE 
 class Solution {
     public int[] arrayChange(int[] nums, int[][] operations) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
-
-        for(int i=0;i<operations.length;i++){
-            if(map.containsKey(operations[i][0])){
-                int freq = map.get(operations[i][0]);
-                map.put(operations[i][1],freq);
-                map.remove(operations[i][0]);
-            }
-        }
-        
-        int ans[] = new int[nums.length];
-        for(int key: map.keySet()){
-            int idx = map.get(key);
-            ans[idx] = key;
-        }
-        return ans;
+      HashMap<Integer,Integer> map = new HashMap<>();
+      for(int i=0;i<nums.length;i++){
+        map.put(nums[i],i);
+      }  
+      for(int i=0;i<operations.length;i++){
+        int oldvalue = operations[i][0];
+        int newvalue = operations[i][1];
+        int idx = map.get(oldvalue);
+        nums[idx] = newvalue;
+        map.remove(oldvalue);
+        map.put(newvalue,idx);
+      }
+      return nums;
     }
 }
