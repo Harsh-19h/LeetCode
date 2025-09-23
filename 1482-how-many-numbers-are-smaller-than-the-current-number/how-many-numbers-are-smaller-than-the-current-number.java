3c381f36cd457301586dd1744bd091c0c8ba50f3
Harsh Kumar <harshofficial1905@gmail.com>
2025-09-23 23:28:14 +0530

    Time: 6 ms (73.93%) | Memory: 44.8 MB (23.90%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,19 +1,24 @@
+// OPTIMIZED APPROACH
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int arr[] = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            arr[i]= nums[i];
+     int arr[] = new int[nums.length];
+     for(int i=0;i<nums.length;i++){
+        arr[i] = nums[i];
+     }   
+     Arrays.sort(arr);
+     HashMap<Integer,Integer> map = new HashMap<>();
+     for(int i=0;i<arr.length;i++){
+        if(!map.containsKey(arr[i])){
+            map.put(arr[i],i);
         }
-        Arrays.sort(arr);
-        int ans[] = new int[arr.length];
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(nums[i]==arr[j]) {
-                    ans[i] =j;
-                    break;
-                }
-            }
+     }
+     int ans[] = new int[arr.length];
+     for(int i=0;i<nums.length;i++){
+        if(map.containsKey(nums[i])){
+            int idx = map.get(nums[i]);
+            ans[i] = idx;
         }
-        return ans;
+     }
+     return ans;
     }
 }
