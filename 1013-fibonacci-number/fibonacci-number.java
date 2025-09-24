50c63709ca206929e635b990d08e71a2f48c0631
Harsh Kumar <harshofficial1905@gmail.com>
2025-09-24 12:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (6.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,6 +1,13 @@
+// DP OPTIMIZED APPROACH
 class Solution {
-    public int fib(int n) {
+    public int helper(int n, int dp[]){
         if(n<=1) return n;
-      return fib(n-1) + fib(n-2);  
+        if(dp[n]!=0) return dp[n];
+        dp[n] = helper(n-1,dp) + helper(n-2,dp);
+        return helper(n-1,dp) + helper(n-2,dp);
+    }
+    public int fib(int n) {
+        int dp[] = new int[n+1];
+        return helper(n,dp);
     }
 }
