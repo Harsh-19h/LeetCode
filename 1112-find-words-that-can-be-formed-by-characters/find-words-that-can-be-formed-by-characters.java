3a1baddc833e61c50ab98b43dce2886953abc941
Harsh Kumar <harshofficial1905@gmail.com>
2025-10-17 19:48:23 +0530

    Time: 6 ms (77.27%) | Memory: 45.3 MB (49.94%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,26 +1,21 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<chars.length();i++){
-            map.put(chars.charAt(i),map.getOrDefault(chars.charAt(i),0)+1);
+        int freq[] = new int[26];
+        for(char ch: chars.toCharArray()){
+            freq[ch-'a']++;
         }
-        int ans =0;
-        for(int i=0;i<words.length;i++){
-            String curr = words[i];
+        int ans = 0;
+        for(String st: words){
+            int temp[] = new int[26];
             boolean present = true;
-            HashMap<Character,Integer> map2 = new HashMap<>(map);
-            for(int j=0;j<curr.length();j++){
-                if(map2.containsKey(curr.charAt(j))){
-                    int freq = map2.get(curr.charAt(j));
-                    if(freq==1) map2.remove(curr.charAt(j));
-                    else map2.put(curr.charAt(j),freq-1);
-
-                }else {
+            for(char ch: st.toCharArray()){
+                temp[ch-'a']++;
+                if(temp[ch-'a']>freq[ch-'a']){
                     present = false;
                     break;
                 }
             }
-            if(present==true) ans+=curr.length();
+            if(present ==true)  ans+=st.length();
         }
         return ans;
     }
