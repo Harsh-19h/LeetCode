537b838f48c930b83391c7cddc09f5da04afafa6
Harsh Kumar <harshofficial1905@gmail.com>
2025-10-24 15:50:17 +0530

    Time: 93 ms (11.51%) | Memory: 56.7 MB (90.02%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -1,29 +1,23 @@
+// OPTIMIZED APPROACH
 class Solution {
-    class pair implements Comparable<pair>{
-        int val;
-        int freq;
-        pair(int val, int freq){
-            this.val = val;
-            this.freq = freq;
-        }
-        public int compareTo(pair p){
-            return this.freq-p.freq;
-        }
-    }
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+
+     HashMap<Integer,Integer> map = new HashMap<>();
       for(int i=0;i<arr.length;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
       }
-      PriorityQueue<pair> pq = new PriorityQueue<>();
-      for(int key: map.keySet()){
-        pq.add(new pair(key,map.get(key)));
+
+      PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> map.get(a)-map.get(b));
+      pq.addAll(map.keySet());
+
+      while(k>0 && !pq.isEmpty()){
+        int top = pq.poll();
+        int freq = map.get(top);
+        if(freq<=k){
+           k-=freq;
+           map.remove(top); 
+        } else break;
       }
-      while(k>0){
-        pair curr = pq.remove();
-        if(curr.freq>1) pq.add(new pair(curr.val,curr.freq-1));
-        k--;
-      }
-      return pq.size();
+      return map.size();
     }
 }
