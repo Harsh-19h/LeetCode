f2c9218fcccebad122b7519c399945ae1d01d087
Harsh Kumar <harshofficial1905@gmail.com>
2025-10-30 10:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (76.37%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,24 +1,16 @@
 class Solution {
-    public void bfs(int idx ,List<List<Integer>> rooms, boolean canopen[]){
+    public void dfs(int idx ,List<List<Integer>> rooms, boolean canopen[]){
         canopen[idx] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(idx);
-        while(!q.isEmpty()){
-            int front = q.poll();
-            for(int i=0;i<rooms.get(front).size();i++){
-                int curr = rooms.get(front).get(i);
-                if(canopen[curr]==false){
-                q.add(curr);
-                canopen[curr] = true;
-                }
-            }
+        for(int i=0;i<rooms.get(idx).size();i++){
+            int curr = rooms.get(idx).get(i);
+            if(!canopen [curr]) dfs(curr,rooms,canopen);
         }
     }
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean ans = false;
         int n = rooms.size();
         boolean canopen[] = new boolean[n];
-        bfs(0,rooms,canopen);
+        dfs(0,rooms,canopen);
         for(int i=0;i<canopen.length;i++){
             if(canopen[i]==false) return false;
         }
