9b5c91017029f0d1582238dc0270766f622add67
Harsh Kumar <harshofficial1905@gmail.com>
2025-10-30 10:44:27 +0530

    Time: 3 ms (84.24%) | Memory: 49.5 MB (72.51%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,38 +1,19 @@
-class Solution { 
-    class pair{
-        int row;
-        int col;
-        pair(int row ,int col){
-            this.row = row ;
-            this.col = col;
-        }
-    }
-    public void bfs(boolean visited[][] , char grid[][],int i, int j ){
-        Queue<pair> q = new LinkedList<>();
-        q.add(new pair(i,j));
+// (DFS) APPROACH
+class Solution {
+   public void dfs(boolean visited[][] , char grid[][],int i, int j ){
         visited[i][j] = true;
-        while(!q.isEmpty()){
-            pair curr = q.poll();
-            int r = curr.row;
-            int c = curr.col;
-            
-            if(c>0 && grid[r][c-1]=='1' && visited[r][c-1]==false){
-                q.add(new pair(r,c-1));
-                visited[r][c-1] = true;
-            } 
-            if(c<grid[0].length-1 && grid[r][c+1]=='1' && visited[r][c+1]==false){
-                q.add(new pair(r,c+1));
-                visited[r][c+1] = true;
-            } 
-            if(r<grid.length-1 && grid[r+1][c]=='1' && visited[r+1][c]==false){
-                q.add(new pair(r+1,c));
-                visited[r+1][c] = true;
-            }
-            if(r>0 && grid[r-1][c]=='1' && visited[r-1][c]==false){
-                 q.add(new pair(r-1,c)); 
-                 visited[r-1][c] = true;
-            }   
+        if(j>0 && grid[i][j-1]=='1' && visited[i][j-1]==false){
+            dfs(visited, grid,i,j-1);
+        } 
+        if(j<grid[0].length-1 && grid[i][j+1]=='1' && visited[i][j+1]==false){
+            dfs(visited,grid,i,j+1);
+        } 
+        if(i<grid.length-1 && grid[i+1][j]=='1' && visited[i+1][j]==false){
+            dfs(visited,grid,i+1,j);
         }
+        if(i>0 && grid[i-1][j]=='1' && visited[i-1][j]==false){ 
+            dfs(visited,grid,i-1,j);
+        }   
     }
     public int numIslands(char[][] grid) {
         int count = 0;
@@ -40,7 +21,7 @@ class Solution {
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++){
                 if(grid[i][j]=='1' && visited[i][j]==false){
-                    bfs(visited,grid,i,j);
+                    dfs(visited,grid,i,j);
                     count++;
                 }
             }
