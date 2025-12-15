1d1247913e911183ac6e2b0e9b42be63e38fb5fa
Harsh Kumar <harshofficial1905@gmail.com>
2025-12-15 14:52:40 +0530

    Time: 1 ms (99.63%) | Memory: 45 MB (71.00%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,24 +1,32 @@
 class Solution {
-    public int compress(char[] chars) {
-        int k=0;
+    public int compress(char[] s) {
+        int k =0;
         int count =1;
-        for(int i=0;i<chars.length-1;i++){
-            if(chars[i]!=chars[i+1]){
-                chars[k++] = chars[i];
-                if(count>1){
-                    String s = String.valueOf(count);
-                    for(char c: s.toCharArray()) chars[k++] = c;
-                } 
-                count=1;
-            } else{
+        for(int i=0;i<s.length-1;i++){
+            if(s[i]==s[i+1]){
                 count++;
+            } else{
+                s[k++] = s[i];
+                if(count>=10){
+                    String temp = String.valueOf(count);
+                    for(char c: temp.toCharArray()){
+                        s[k++] = c;
+                    }
+                }
+                else if (count>1 && count<10){
+                    s[k++] = (char) (count +'0');
+                }
+                count=1;
             }
         }
-        chars[k++]=chars[chars.length-1];
-       if(count>1){
-            String s = String.valueOf(count);
-            for(char c: s.toCharArray()) chars[k++] = c;
-        } 
+        s[k++] = s[s.length-1];
+        if(count>1 && count<10)  s[k++] = (char) (count +'0');
+        if(count>=10){
+            String temp = String.valueOf(count);
+            for(char c: temp.toCharArray()){
+                s[k++] = c;
+            }
+        }
         return k;
     }
 }
