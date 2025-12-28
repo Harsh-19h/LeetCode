7e0fbdde478b882af249330927682292a66d8aae
Harsh Kumar <harshofficial1905@gmail.com>
2025-12-28 16:18:01 +0530

    Time: 481 ms (25.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+Select d.name as Department,
+e.name as Employee,
+e.salary as Salary
+from Employee as e
+join Department as d
+on e.departmentId = d.id
+where e.salary = (select max(salary) from Employee where departmentId = e.departmentId)
