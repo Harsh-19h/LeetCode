1fcbc6377497d1e0e2bb81753899fca5933868be
Harsh Kumar <harshofficial1905@gmail.com>
2026-01-02 13:36:53 +0530

    Time: 600 ms (55.21%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
 select distinct user_id,
-count(follower_id) over (partition by user_id) as followers_count
+count(user_id) as followers_count
 from Followers
-group by user_id,follower_id
+group by user_id
 order by user_id
 
