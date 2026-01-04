0e3842223cfc4119b7380d9752f3ce38875c4a47
Harsh Kumar <harshofficial1905@gmail.com>
2026-01-04 14:40:18 +0530

    Time: 4 ms (99.64%) | Memory: 225.4 MB (81.73%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -1,20 +1,20 @@
+// optimize in-place way
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-       ListNode temp = head.next;
-       int sum =0;
-       ListNode ans = new ListNode(-1);
-       ListNode temp2 = ans;
+        ListNode ans = head;
+        ListNode curr = head.next;
+        int sum =0;
 
-       while(temp!=null){
-        if(temp.val!=0){
-            sum+=temp.val;
-        }else {
-            temp2.next = new ListNode (sum);
-            temp2 = temp2.next;
-            sum =0;
+        while(curr!=null){
+            if(curr.val!=0) sum+=curr.val;
+            else{
+                ans.val = sum;
+                ans.next = curr.next;
+                ans = ans.next;
+                sum =0;
+            }
+            curr = curr.next;
         }
-        temp = temp.next;
-       }
-       return ans.next; 
+        return head;
     }
 }
