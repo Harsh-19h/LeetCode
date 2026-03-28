33e827ba265d5693e940f9c5121a68cfdd5ae61f
Harsh Kumar <harshofficial1905@gmail.com>
2026-03-28 15:52:43 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (68.92%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,19 +1,13 @@
 class Solution {
-    public static boolean bfs(int visited[], int color[] , int start, int graph[][]){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visited[start] = 1;
-        color[start] = 0;
-        while(!q.isEmpty()){
-            int curr = q.remove();
-            for(int val : graph[curr]){
-                if(visited[val]==1){
-                    if(color[val] == color[curr]) return false;
-                }else{
-                    visited[val] =1;
-                    q.add(val);
-                    color[val] = (color[curr]==1)?0:1;
-                }
+    public static boolean dfs(int visited[], int color[] , int start, int graph[][]){
+       for(int val: graph[start]){
+            if(visited[val]==1){
+                if(color[val]==color[start]) return false;
+            }
+            else{
+                visited[val] =1;
+                color[val] = (color[start]==1)?0:1;
+                if(!dfs(visited,color,val,graph)) return false;
             }
         }
         return true;
@@ -24,7 +18,8 @@ class Solution {
         Arrays.fill(color,-1);
         for(int i=0;i<graph.length;i++){
             if(visited[i]==0){
-                if(!bfs(visited,color,i,graph)) return false;
+                visited[i] = 1; color[i] =0;
+                if(!dfs(visited,color,i,graph)) return false;
             }
         }
         return true;
