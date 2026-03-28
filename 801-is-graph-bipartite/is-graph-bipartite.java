a4775bcdacd04e48f497b0496a05eb934b6d4323
Harsh Kumar <harshofficial1905@gmail.com>
2026-03-28 15:38:25 +0530

    Time: 1 ms (64.08%) | Memory: 47.3 MB (29.18%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,30 +1,30 @@
 class Solution {
-    public boolean bfs(int visited[], int[][] graph,int i){
+    public static boolean bfs(int visited[], int color[] , int start, int graph[][]){
         Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        visited[i] = 1;
+        q.add(start);
+        visited[start] = 1;
+        color[start] = 0;
         while(!q.isEmpty()){
             int curr = q.remove();
-            for(int j=0;j<graph[curr].length;j++){
-                int c = graph[curr][j];
-                if(visited[c]==-1){
-                    q.add(c);
-                     if(visited[curr]==1) visited[c] = 0;
-                    else if(visited[curr]==0) visited[c]=1;
+            for(int val : graph[curr]){
+                if(visited[val]==1){
+                    if(color[val] == color[curr]) return false;
                 }else{
-                    if(visited[curr]==visited[c]) return false;
-                }    
+                    visited[val] =1;
+                    q.add(val);
+                    color[val] = (color[curr]==1)?0:1;
+                }
             }
         }
         return true;
     }
     public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        int visited[] =new int[n];
-        Arrays.fill(visited,-1);
-        for(int i=0;i<n;i++){
-            if(visited[i]==-1){
-                if(!bfs(visited,graph,i)) return false;
+        int visited[] = new int[graph.length];
+        int color[]  = new int [graph.length];
+        Arrays.fill(color,-1);
+        for(int i=0;i<graph.length;i++){
+            if(visited[i]==0){
+                if(!bfs(visited,color,i,graph)) return false;
             }
         }
         return true;
