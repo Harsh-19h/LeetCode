9dfa477c4ffe2da8ddfa38d076e756336eb8f641
Harsh Kumar <harshofficial1905@gmail.com>
2026-04-03 13:21:27 +0530

    Time: 4 ms (99.81%) | Memory: 65.6 MB (8.70%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,37 +1,31 @@
 class Solution {
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-     List<List<Integer>> list = new ArrayList<>();
-     for(int i=0;i<graph.length;i++){
-        list.add(new ArrayList<>());
-     }
-     int outdegree[] = new int[graph.length];   
-     for(int i=0;i<graph.length;i++){
-        for(int j=0;j<graph[i].length;j++){
-            int a = graph[i][j];
-            list.get(a).add(i);
-            outdegree[i]++;
-        }
-     }
-     ArrayList<Integer> ans = new ArrayList<>();
-     Queue<Integer> q = new LinkedList<>();
-     for(int i=0;i<graph.length;i++){
-        if(outdegree[i]==0){
-            q.add(i);
-            ans.add(i);
-        } 
-     }
-     while(!q.isEmpty()){
-        int top = q.remove();
-        for(int i=0;i<list.get(top).size();i++){
-            int curr = list.get(top).get(i);
-            outdegree[curr]--;
-            if(outdegree[curr]==0) {
-                q.add(curr);
-                ans.add(curr);
+    public boolean dfs(int[] visited, int[] pathvi,int safe[], int[][] graph,int start){
+        visited[start] = 1;
+        pathvi [start] =1;
+        for(int val: graph[start]){
+            if(visited[val]==1){
+                if(pathvi[val]==1) return true;
+            } else{
+                if(dfs(visited,pathvi,safe,graph,val)==true) return true;
             }
         }
-     }
-      Collections.sort(ans);
-      return ans;
+        safe[start] = 1;
+        pathvi[start] = 0;
+        return false;
+    }
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int visited[] = new int[graph.length];
+        int pathvi[] = new int[graph.length];
+        int safe[] = new int[graph.length];
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<graph.length;i++){
+            if(visited[i]==0){
+                dfs(visited,pathvi,safe,graph,i);
+            }
+        }
+        for(int i=0;i<graph.length;i++){
+            if(safe[i]==1) ans.add(i);
+        }
+        return ans;
     }
 }
