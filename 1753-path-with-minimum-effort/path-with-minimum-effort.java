e3ea294d264888061e7ce0b79961f0436f8eae35
Harsh Kumar <harshofficial1905@gmail.com>
2026-04-08 13:28:40 +0530

    Time: 61 ms (19.81%) | Memory: 47.5 MB (96.58%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,68 +1,58 @@
 class Solution {
-    class triplet implements Comparable<triplet> {
-        int row;
-        int col;
-        int effort;
-        triplet(int row,int col,int effort){
-            this.row = row;
-            this.col = col;
-            this.effort = effort;
-        }
-        public int compareTo(triplet t){
-            if(this.effort == t.effort) return this.row -t.row;
-            return this.effort - t.effort;
-        }
-    }
-    public int minimumEffortPath(int[][] arr) {
-        int r = arr.length;
-        int c = arr[0].length;
-        int ans[][] = new int[r][c];
-        for(int i=0;i<r;i++){
+    public int minimumEffortPath(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+        int ans[][]  = new int[n][m];
+
+        for(int i=0;i<n;i++){
             Arrays.fill(ans[i],Integer.MAX_VALUE);
         }
         ans[0][0] = 0;
-        PriorityQueue<triplet> pq = new PriorityQueue<>();
-        pq.add(new triplet(0,0,0));
-        while(!pq.isEmpty()){
-            triplet curr = pq.remove();
-            int row = curr.row;
-            int col =curr.col;
-            int effort = curr.effort;
-            if(row==r-1 && col==c-1) break;
-            if(row>0){  
-                int e = Math.abs(arr[row][col]-arr[row-1][col]);
-                e = Math.max(effort,e);
-                if(e<ans[row-1][col]){
-                    ans[row-1][col]=e;
-                    pq.add(new triplet(row-1,col,e));
-                }
-            }
-            if(col>0){
-                int e = Math.abs(arr[row][col]-arr[row][col-1]);
-                e = Math.max(effort,e);
-                if(e<ans[row][col-1]){
-                    ans[row][col-1]=e;
-                    pq.add(new triplet(row,col-1,e));
-                }
-            }
-             if(row<r-1){
-                int e = Math.abs(arr[row][col]-arr[row+1][col]);
-                e = Math.max(effort,e);
-                if(e<ans[row+1][col]){
-                    ans[row+1][col]=e;
-                    pq.add(new triplet(row+1,col,e));
-                }
-            }
-            if(col<c-1){
-                int e = Math.abs(arr[row][col]-arr[row][col+1]);
-                e = Math.max(effort,e);
-                if(e<ans[row][col+1]){
-                    ans[row][col+1]=e;
-                    pq.add(new triplet(row,col+1,e));
-                }
-            }
 
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y)-> x[0]-y[0]);
+        pq.add(new int[]{0,0,0});
+
+        while(!pq.isEmpty()){
+            int curr[] = pq.remove();
+            int effort = curr[0];
+            int i=curr[1];
+            int j=curr[2];
+            if(i==n-1 && j==m-1) break;
+
+            if(i>0){
+                int e = Math.abs(heights[i][j] - heights[i-1][j]);
+                e = Math.max(e,effort);
+                if(e<ans[i-1][j]){
+                    ans[i-1][j] = e;
+                    pq.add(new int[]{e,i-1,j});
+                }
+            }
+            if(i<n-1){
+                int e = Math.abs(heights[i][j]- heights[i+1][j]);
+                e = Math.max(e,effort);
+                if(e<ans[i+1][j]){
+                    ans[i+1][j] = e;
+                   pq.add(new int[]{e,i+1,j});
+                }
+            }
+            if(j>0){
+                int e = Math.abs(heights[i][j] - heights[i][j-1]);
+                e = Math.max(e,effort);
+                if(e<ans[i][j-1]){
+                    ans[i][j-1] = e;
+                   pq.add(new int[]{e,i,j-1});
+                }
+            }
+            if(j<m-1){
+                int e = Math.abs(heights[i][j]- heights[i][j+1]);
+                e = Math.max(e,effort);
+                if(e<ans[i][j+1]){
+                    ans[i][j+1] = e;
+                    pq.add(new int[]{e,i,j+1});
+                }
+            }
         }
-        return ans[r-1][c-1];
+        return ans[n-1][m-1];
+
     }
 }
