346de6b9b9b01070705c9c190533a40deef62409
Harsh Kumar <harshofficial1905@gmail.com>
2026-04-28 09:05:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (11.93%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,32 +1,30 @@
 class Solution {
-    public static int[] reverse(int[] arr, int left , int right){
-        while(left<right){
-            int temp = arr[left];
-            arr [left]=arr[right];
-            arr[right]=temp;
-            left++;
-            right--;
+    public void reverse(int nums[], int i ,int j){
+        while(i<j){
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+
+            i++;
+            j--;
         }
-        return arr;
+        return ;
     }
     public boolean check(int[] nums) {
-        int n= nums.length;
-        int k=0;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]<=nums[i+1]){
-                k++;
-            }else{
+        int idx = -1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                idx = i;
                 break;
             }
         }
-        int x = n-k-1;
-        reverse(nums,0,k);
-        reverse(nums,k+1,n-1);
-        reverse(nums,0,n-1);
-        for(int i=1;i<n;i++){
-            if(nums[i]<nums[i-1]){
-                return false;
-            }
+        if(idx==-1) return true;
+        reverse(nums,0,idx-1);
+        reverse(nums,idx,nums.length-1);
+        reverse(nums,0,nums.length-1);
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]) return false;
         }
         return true;
     }
