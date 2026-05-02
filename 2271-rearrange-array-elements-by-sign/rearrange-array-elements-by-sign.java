bd8afb177dedd664c005b7ab4151b4d90c791def
Harsh Kumar <harshofficial1905@gmail.com>
2026-05-02 21:17:51 +0530

    Time: 4 ms (42.04%) | Memory: 175.1 MB (75.98%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,18 +1,18 @@
-// 2 pointer (optimzed O(1)space)
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-      int pi = 0;
-      int ni = 1;
-      int ans[] = new int[nums.length];
-      for(int i=0;i<nums.length;i++){
-        if(nums[i]>0){
-            ans[pi] = nums[i];
-            pi+=2;
-        } else{
-            ans[ni] = nums[i];
-            ni+=2;
+        int i = 0;
+        int j = 1;
+        int ans[] = new int[nums.length];
+        if(nums.length==1) return nums;
+        for(int val: nums){
+            if(val>0){
+                ans[i] = val;
+                i+=2;
+            }else{
+                ans[j] = val;
+                j+=2;
+            }
         }
-      } 
-      return ans; 
+        return ans;
     }
 }
