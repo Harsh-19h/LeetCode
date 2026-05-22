33b11a0759cf7cba188dbf8f69fa0aacaa168512
Harsh Kumar <harshofficial1905@gmail.com>
2026-05-22 16:16:22 +0530

    Time: 2 ms (99.92%) | Memory: 52 MB (85.36%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,18 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                int freq = map.get(nums[i]);
-                map.put(nums[i],freq+1);
-            } else map.put(nums[i],1);
+        List<Integer> ans = new ArrayList<>();
+
+        int a = 0, b = 0;
+        int freq_a = 0, freq_b = 0;
+
+        for(int num : nums) {
+
+            if(num == a) {
+                freq_a++;
+            }
+
+            else if(num == b) {
+                freq_b++;
+            }
+
+            else if(freq_a == 0) {
+                a = num;
+                freq_a = 1;
+            }
+
+            else if(freq_b == 0) {
+                b = num;
+                freq_b = 1;
+            }
+
+            else {
+                freq_a--;
+                freq_b--;
+            }
         }
-        int limit =(int) Math.floor(nums.length/3);
-        ArrayList<Integer>ans = new ArrayList<>();
-        for(int key: map.keySet()){
-            int val = map.get(key);
-            if(val>limit) ans.add(key);
+
+        freq_a = 0;
+        freq_b = 0;
+
+        for(int num : nums) {
+            if(num == a) freq_a++;
+            else if(num == b) freq_b++;
         }
+
+        if(freq_a > nums.length / 3) ans.add(a);
+        if(freq_b > nums.length / 3) ans.add(b);
+
         return ans;
     }
 }
