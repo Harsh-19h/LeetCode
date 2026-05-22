228e0eead2f25f41527c357a4fa4143b0d90081a
Harsh Kumar <harshofficial1905@gmail.com>
2026-05-22 19:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (63.65%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,20 +1,37 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int start = 0 ;
-        int end = nums.length-1;
+
+    public int bs(int nums[],int start, int end,int target){
         while(start<=end){
-            int mid = (start+end)/2;
+             int mid = (start + end)/2;
              if(nums[mid]==target) return mid;
-            if(nums[start]<=nums[mid]){
-               if(nums[start]<=target && target<nums[mid]) end = mid-1;
-               else start = mid+1;
-            }else{
-                if(nums[mid]<target && target<=nums[end]) start = mid+1;
-                else{
-                    end = mid-1;
-                }
-            }
+             else if(nums[mid]<target)start++;
+             else end--;
         }
         return -1;
+
+    }
+    public int search(int[] nums, int target) {
+        int ans = -1;
+
+        if(nums.length==1){
+            return (nums[0]==target)?0:-1;
+        }
+
+        int idx = -1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]) {
+                idx = i;
+                break;
+            }
+        }
+
+        if(idx == -1) return bs(nums,0,nums.length-1,target);
+
+        else{
+            ans = bs(nums,0,idx,target);
+            if(ans==-1) ans = bs(nums,idx+1,nums.length-1,target);
+        }
+
+        return ans;
     }
 }
