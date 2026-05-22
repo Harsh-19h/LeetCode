a96f0131eef8d114ac1363b664c0632f0498994b
Harsh Kumar <harshofficial1905@gmail.com>
2026-05-22 19:57:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (80.27%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -4,8 +4,8 @@ class Solution {
         while(start<=end){
              int mid = (start + end)/2;
              if(nums[mid]==target) return mid;
-             else if(nums[mid]<target)start++;
-             else end--;
+             else if(nums[mid]<target)start = mid+1;
+             else end = mid-1;
         }
         return -1;
 
