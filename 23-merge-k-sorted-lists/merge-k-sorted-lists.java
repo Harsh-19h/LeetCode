46c28190e02b40c5168db3800127760201e82b0b
Harsh Kumar <harshofficial1905@gmail.com>
2026-06-12 23:38:52 +0530

    Time: 4 ms (82.02%) | Memory: 46.7 MB (78.03%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,24 +1,31 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->a.val-b.val);
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)-> a.val-b.val);
         for(int i=0;i<lists.length;i++){
-         ListNode curr = lists[i];
-            while(curr!=null){
-                pq.add(curr);
-                curr =  curr.next;
-            }
-        } 
-        if(pq.isEmpty()) return null;
-           
-        ListNode head = new ListNode(0);
-        ListNode temp = head;
-        while(!pq.isEmpty()){
-            ListNode node = pq.poll();
-            node.next = null;
-            temp.next = node;
-            temp = temp.next;
+            if(lists[i]!=null) pq.add(lists[i]);
         }
-        return head.next;
+
+        if(pq.isEmpty()) return null;
+
+        ListNode dummy = new ListNode(0);
+        ListNode start = dummy;
+        while(!pq.isEmpty()){
+            ListNode curr = pq.remove();
+            if(curr.next!=null) pq.add(curr.next);
+            start.next = curr;
+            start = start.next;
+        }
+
+        return dummy.next;
     }
 }
