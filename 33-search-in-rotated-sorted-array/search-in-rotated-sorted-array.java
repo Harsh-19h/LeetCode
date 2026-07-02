50b5e2638d663f53085008ea2a96cc1c18f5e43f
Harsh Kumar <harshofficial1905@gmail.com>
2026-07-02 14:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (64.87%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,37 +1,21 @@
 class Solution {
-
-    public int bs(int nums[],int start, int end,int target){
-        while(start<=end){
-             int mid = (start + end)/2;
-             if(nums[mid]==target) return mid;
-             else if(nums[mid]<target)start = mid+1;
-             else end = mid-1;
-        }
-        return -1;
-
-    }
     public int search(int[] nums, int target) {
-        int ans = -1;
+        int s = 0;
+        int e = nums.length-1;
+        while(s<=e){
+            int mid = s +(e-s)/2;
+            if(nums[mid]== target) return mid;
 
-        if(nums.length==1){
-            return (nums[0]==target)?0:-1;
-        }
-
-        int idx = -1;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]) {
-                idx = i;
-                break;
+            if(nums[s] <= nums[mid]){
+                if(target<nums[mid] && target>= nums[s] ) e = mid-1;
+                else if (target<nums[mid] && target< nums[s]) s = mid+1;
+                else s = mid+1;
+            } else {
+                if(target > nums[mid] && target>=nums[s]) e = mid-1;
+                else if(target>nums[mid] && target<nums[s]) s = mid+1;
+                else e = mid-1;
             }
         }
-
-        if(idx == -1) return bs(nums,0,nums.length-1,target);
-
-        else{
-            ans = bs(nums,0,idx,target);
-            if(ans==-1) ans = bs(nums,idx+1,nums.length-1,target);
-        }
-
-        return ans;
+        return -1;
     }
 }
