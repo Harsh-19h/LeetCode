618c6aec457ef37c6f9e0953732595f59efdb64e
Harsh Kumar <harshofficial1905@gmail.com>
2026-07-12 13:08:47 +0530

    Time: 7 ms (4.98%) | Memory: 47.1 MB (6.38%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,21 +1,41 @@
 class Solution {
     public int romanToInt(String s) {
-     HashMap<Character,Integer> map = new HashMap<>();
-     map.put('I',1);
-     map.put('V',5);
-     map.put('X',10);
-     map.put('L',50);
-     map.put('C',100);
-     map.put('D',500);
-     map.put('M',1000);
-     int ans = 0;
-     for(int i=0;i<s.length();i++){
-        char ch = s.charAt(i);
-        int val = map.get(ch);
-        if(i+1<s.length() && val< map.get(s.charAt(i+1))){
-            ans-=val;
-        } else ans+=val;
-     } 
-     return ans;  
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("I",1);
+        map.put("V",5);
+        map.put("X",10);
+        map.put("L",50);
+        map.put("C",100);
+        map.put("D",500);
+        map.put("M",1000);
+        map.put("IV",4);
+        map.put("IX",9);
+        map.put("XL",40);
+        map.put("XC",90);
+        map.put("CD",400);
+        map.put("CM",900);
+
+        int ans = 0;
+        int i=0,j=1;
+
+        if(s.length()==1){
+            ans+= map.get(s.substring(0));
+            return ans;
+        }
+        while(j<s.length()){
+            String curr = s.substring(i,j+1);
+            if(map.containsKey(curr)){
+                ans+=map.get(curr);
+                i=i+2; j= j+2;
+            }
+            else {
+                ans += map.get(s.substring(i,j));
+                i++;j++;
+            }
+        }
+        if(!map.containsKey(s.substring(s.length()-2))){
+            ans+= map.get(s.substring(s.length()-1));
+        }
+        return ans;
     }
 }
